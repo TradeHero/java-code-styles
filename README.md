34004f6735277954de36796ada8085661749f32d
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2013-08-13 18:26:57 +0800

    Checkstyle && IntelliJ coding styles for TradeHero

---
+++ README.md
@@ -0,0 +1,16 @@
+Java Code Styles
+================
+
+IntelliJ IDEA code style settings for TradeHero's Java projects.
+
+
+Installation
+------------
+
+ * Run the `install.sh` script.
+ * Restart IntelliJ if it's running.
+ * Open IntelliJ Project Settings -> Code Styles, change the code style for the
+   project to the one you want.
+
+
+Credit: Installation script is from SquareUp.
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Installs TradeHero's IntelliJ configs into your user configs.
+
+echo "Installing Square code style configs..."
+
+for i in $HOME/Library/Preferences/IntelliJIdea*/codestyles \
+         $HOME/Library/Preferences/IdeaIC*/codestyles \
+         $HOME/Library/Preferences/AndroidStudio*/codestyles
+do
+  cp -frv $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/configs/* $i 2> /dev/null
+done
+
+echo "Done."
+echo ""
+echo "Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'TradeHero'.
